290389e8942f2998000e131391bae1dc9fe20df0
AkDzmitry <akdzmitry@gmail.com>
2020-01-26 21:38:24 -0700

    Step 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: AppBar(
-            title: Text('Ask Me Anything'),
-            backgroundColor: Colors.blue.shade900,
-          ),
-        )
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
       ),
     );
+  }
+}
