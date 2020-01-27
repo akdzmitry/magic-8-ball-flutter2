9045ebe013711c284257c2c008280790eadf3aa9
AkDzmitry <akdzmitry@gmail.com>
2020-01-26 21:16:06 -0700

    Step 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+        )
       ),
     );
